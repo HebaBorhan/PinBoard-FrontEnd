5c2ed9ea36e73e7ed6d6e922d9b04d4ee9815742
HebaZaki <h_borhan@hotmail.com>
2024-10-15 11:34:31 +0300

    registration button

--- dashboard/src/LandingPage/LandingPage.js
+++ dashboard/src/LandingPage/LandingPage.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "./logo.png";
 
 function LandingPage() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Welcome to PinBoard</h1>
-      <p>Hold That Thought!</p>
+      <img 
+        src={logo} 
+        alt="PinBoard Logo" 
+        style={{ width: "300px", height: "auto", marginBottom: "20px" }}
+      />
+      <p style={{ fontSize: "24px", fontWeight: "bold" }}>
+        Hold That Thought!
+      </p>
       <Link to="/login">
+        <button style={{ padding: "10px 20px", marginTop: "20px" }}>
+          Log In
+        </button>
+      </Link>
+      <Link to="/registration">
         <button style={{ padding: "10px 20px", marginTop: "20px" }}>
           Log In
         </button>
