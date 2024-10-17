598ec8e66f910d272e0c7e1573f5c23f9ce284be
HebaZaki <h_borhan@hotmail.com>
2024-10-17 12:13:27 +0300

    offline use

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -10,6 +10,16 @@ root.render(
   </React.StrictMode>
 );
 
+// Add a service worker to cache files for offline use
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.error('Service Worker registration failed:', err));
+  });
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
