a5cab01f8c8ca0030731145d5074c63024fac378
HebaZaki <h_borhan@hotmail.com>
2024-10-17 11:42:09 +0300

    update fields

--- dashboard/src/api/apiClient.js
+++ dashboard/src/api/apiClient.js
@@ -5,12 +5,15 @@ const baseUrl = 'http://localhost:5000'
 const loginEndpoint = `${baseUrl}/login`
 const getUserEndpoint = `${baseUrl}/users/get`
 const createNote = `${baseUrl}/notes`
+const registerUser = `${baseUrl}/register`
+const getNote = `${baseUrl}/notes`
+const cook = cookies.get('token');
 
 
 export default class ApiClient {
     static login = async (userEmail, password) => {
         console.log("calling backend")
-        const body = { userEmail, password };
+        const body = { email: userEmail, password };
         const response = await fetch(loginEndpoint, {
             method: 'post',
             body: JSON.stringify(body),
@@ -28,61 +31,83 @@ export default class ApiClient {
         const response = await fetch(getUserEndpoint, {
             method: 'GET',
             headers: {
-                'x-token': cook && cook.token ? cook.token : '',
+                'token': cook && cook.token ? cook.token : '',
                 'Content-Type': 'application/json',
             }
         });
+        if (!response.ok) console.error('Failed to get user:', await response.json());
         return response;
     }
 
     static register = async (data) => {
-        const response = await fetch(`${baseUrl}/register`, {
+        const response = await fetch(registerUser, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(data),
         });
+        if (!response.ok) {
+            console.error('Failed to register:', await response.json());
+          }
         return response;
     };
 
     static createNote = async (data) => {
-
         const body = { ...data };
         const response = await fetch(createNote, {
             method: 'post',
             body: JSON.stringify(body),
             headers: {
-                'x-token': cookies.get('token'),
+                'token': cook && cook.token ? cook.token : '',
                 'Content-Type': 'application/json',
             }
         });
+        if (!response.ok) console.error('Failed to get user:', await response.json());
         return response;
     }
 
     static getNotes = async () => {
+
         const token = cookies.get('token'); // Retrieve JWT token from cookies
-        const response = await fetch(`${baseUrl}/notes`, {
+        console.log(token)
+        const response = await fetch(getNote, {
             method: 'GET',
+            
             headers: {
                 'Content-Type': 'application/json',
-                'token': token || '',
+                'token': cook && cook.token ? cook.token : '',
             },
         });
+        return response;
+    };
+
+    static updateNote = async (id, updatedData) => {
+        const token = cookies.get('token');
+        const response = await fetch(`${baseUrl}/notes/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'token': cook && cook.token ? cook.token : '',
+            },
+            body: JSON.stringify(updatedData),
+        });
+        if (!response.ok) console.error('Failed to update note:', await response.json());
         return response;
     };
 
     static deleteNote = async (id) => {
         const token = cookies.get('token');
         const response = await fetch(`${baseUrl}/notes/${id}`, {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-            'token': token || '',
-          },
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'token': token || '',
+            },
         });
+        if (!response.ok) console.error('Failed to update note:', await response.json());
         return response;
-      };      
+    };
 
     static logout = () => {
         cookies.remove('token');
