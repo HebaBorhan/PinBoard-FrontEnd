3bf5a07e19f2ac3a6d9d17041efbb902e6830a02
HebaZaki <h_borhan@hotmail.com>
2024-10-17 12:19:55 +0300

    Forgot Password

--- dashboard/src/LandingPage/LandingPage.js
+++ dashboard/src/LandingPage/LandingPage.js
@@ -5,14 +5,20 @@ import logo from "./logo.png";
 function LandingPage() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <img 
-        src={logo} 
-        alt="PinBoard Logo" 
+      <img
+        src={logo}
+        alt="PinBoard Logo"
         style={{ width: "300px", height: "auto", marginBottom: "20px" }}
       />
       <p style={{ fontSize: "24px", fontWeight: "bold" }}>
         Hold That Thought!
       </p>
+      <Link
+        to="/forgot-password"
+        style={{ marginRight: "10px", fontSize: "12px", textDecoration: "underline", color: "#efffa8" }}
+      >
+        Forgot your password?
+      </Link>
       <Link to="/login">
         <button style={{ fontSize: "14px", padding: "10px 20px", marginTop: "20px", marginRight: "20px" }}>
           Log In
